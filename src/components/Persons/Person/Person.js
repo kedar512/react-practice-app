f29edcdc5154eab2e45aa894d6557f68a58b14a3
Kedar Patil <knpatil512@gmail.com>
2021-02-11 21:27:13 +0530

    Added changes for adjacent JSX elements chapter

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+import Aux from '../../../hoc/Auxiliary';
 
 
 class Person extends Component {
 
     render() {
         return(
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>I am {this.props.name} and my age is {this.props.age}</p>
-                {this.props.children}
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            <Aux>
+                <div className={classes.Person}>
+                    <p onClick={this.props.click}>I am {this.props.name} and my age is {this.props.age}</p>
+                    {this.props.children}
+                    <input type="text" onChange={this.props.changed} value={this.props.name} />
+                </div>
+            </Aux>
         );
     }
 }
