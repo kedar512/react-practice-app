03dadc33bf5cfc3463e119c3d538b188b2cb3fef
Kedar Patil <knpatil512@gmail.com>
2021-02-12 21:04:47 +0530

    Added changes for setting state correctly chapter

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
 import Aux from '../../../hoc/Auxiliary';
-
+import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
 
     render() {
         return(
             <Aux>
-                <div className={classes.Person}>
-                    <p onClick={this.props.click}>I am {this.props.name} and my age is {this.props.age}</p>
-                    {this.props.children}
-                    <input type="text" onChange={this.props.changed} value={this.props.name} />
-                </div>
+                <p onClick={this.props.click}>I am {this.props.name} and my age is {this.props.age}</p>
+                {this.props.children}
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
             </Aux>
         );
     }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
--- src/containers/App.js
+++ src/containers/App.js
@@ -18,7 +18,8 @@ class App extends Component {
     ],
     showPersons: false,
     textInput: '',
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   }
 
   switchNameHandler = (newName) => {
@@ -38,7 +39,12 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState( (prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      }
+    });
   }
 
   deletePerson = index => {
